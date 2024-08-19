641c828c138fe5a14f13c2afa9874101ed578a15
Navnath Parte <9nath.parte@gmail.com>
2024-08-19 14:59:35 +0530

    logo fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,10 +12,6 @@ const Footer = () => {
       <div className="footer-top">
         <div className="footer-top-left">
           <img src={footer_logo} alt="" />
-          <p>
-            I am a Node js developer from, India with 2.5 years of experience in
-            company.
-          </p>
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
